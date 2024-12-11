0fe3c152746833743948549851d826dc38bd1c18
gursewak8427 <gursewaksaggu2043@gmail.com>
2024-12-11 21:31:52 +0530

    email service changed to google

--- lib/email.ts
+++ lib/email.ts
@@ -1,39 +1,36 @@
-import nodemailer from "nodemailer"
+import nodemailer from "nodemailer";
 
-const appName = "Ridesewa"
+const appName = "Ridesewa";
 const appColor = '#DA38A7';
 
-export const sendEmail = async ({ email, subject, html }: { email: string, subject: string, html: string }) => {
+export const sendEmail = async ({
+    email,
+    subject,
+    html,
+}: {
+    email: string;
+    subject: string;
+    html: string;
+}) => {
     try {
-        console.log("Sending email...")
+        console.log("Sending email...");
         const transporter = nodemailer.createTransport({
-            host: "in-v3.mailjet.com",
-            port: 587,
-            secure: false, // Use `true` for port 465, `false` for all other ports
-            tls: {
-                maxVersion: 'TLSv1.3',
-                minVersion: 'TLSv1.2',
-                ciphers: 'TLS_AES_128_GCM_SHA256',
-            },
+            service: "gmail",
             auth: {
-                user: "d478a543897eb46f4f8a47d39b19e3c0",
-                pass: "51f611f9afe42a7044dcabe2dc0e87c5",
-            }
+                user: "gursewaksaggu2043@gmail.com", // Replace with your Gmail address
+                pass: "jjenuqkjxlnjrmha", // Replace with your Gmail password or App Password
+            },
         });
 
         const info = await transporter.sendMail({
-            from: `"${appName} Team" <noreply@llamanodes.net>`, // sender address
-            to: email, // list of receivers
+            from: `"${appName} Team" <gursewaksaggu2043@gmail.com>`, // Replace with your Gmail address
+            to: email, // List of receivers
             subject: subject, // Subject line
-            html: html, // html body
+            html: html, // HTML body
         });
 
         console.log("Message sent: %s", info.messageId);
-        // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
     } catch (error) {
-        console.log("Message sent: %s", error);
-
+        console.error("Error sending email:", error);
     }
-
-
-}
+};
